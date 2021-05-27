2d06d4f1b20e3384d58db1e174636d161454e4f0
L-eeX <387799457@qq.com>
2021-05-27 13:43:59 +0800

    lx

--- build/notarize.js
+++ build/notarize.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
-  const appName = context.packager.appInfo.productFilename;
+    const { electronPlatformName, appOutDir } = context;
+    const appName = context.packager.appInfo.productFilename;
 
-  if (electronPlatformName !== 'darwin') {
-    return;
-  }
+    if (electronPlatformName !== 'darwin') {
+        return;
+    }
 
-  return await notarize({
-    appBundleId: 'com.jgraph.drawio.desktop',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
+    return await notarize({
+        appBundleId: 'com.jgraph.dfd.desktop',
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId: process.env.APPLEID,
+        appleIdPassword: process.env.APPLEIDPASS,
+    });
 };
